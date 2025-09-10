29ff043579a4e2530ce930710327cc7802e411c7
melikovgraf1-sys <melikovgraf1@gmail.com>
2025-09-10 14:16:05 +0300

    Add explicit client button to group card

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -22,7 +22,7 @@ export default function NavBar() {
         <Link href="/" className="text-xl font-bold text-blue-700">Judo CRM</Link>
         <nav className="flex gap-2">
           <NavLink href="/">Dashboard</NavLink>
-          <NavLink href="/groups">Groups</NavLink>
+          <NavLink href="/districts">Районы</NavLink>
           <NavLink href="/clients">Clients</NavLink>
           <NavLink href="/leads">Leads</NavLink>
           <NavLink href="/payments">Payments</NavLink>
